3923035ccc84614a66eee70eb9690ed83fabe43a
Cianenry Danan <cianenryd@gmail.com>
2022-08-17 18:55:44 -0700

    Added top 5 guessing game.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 console.log('app is running');
 
+let score = 0;
+
 function getName(){
   let name = prompt('What is your name?');
   console.log(name);
@@ -21,9 +23,10 @@ function getName(){
     case 'no':
     //   console.log('Correct');
       alert('Correct! I was born in Japan!');
+      score = score + 1;
       break;
     case 'yes':
-      alert('Wrong. I was born in Japan.');
+      alert('Nice try! I was born in Japan.');
       break;
     default:
       alert('Yes or no please!');
@@ -40,11 +43,12 @@ function getName(){
     answer = answer.toLowerCase();
     switch(answer){
     case 'yes':
-      alert('Wrong. Im Filipino.');
+      alert('Nice try! Im Filipino.');
       break;
     case 'no':
     //   console.log('Correct');
       alert('Correct. Im Filipino.');
+      score = score + 1;
       break;
     default:
       alert('Yes or No please!');
@@ -61,11 +65,12 @@ function getName(){
     answer = answer.toLowerCase();
     switch(answer){
     case 'no':
-      alert('Wrong. Anything shellfish or sushi related will inhaled by me.');
+      alert('Nice try! Anything shellfish or sushi related will inhaled by me.');
       break;
     case 'yes':
     //   console.log('Correct');
       alert('Correct! Anything shellfish or sushi will be inhaled by me!');
+      score = score + 1;
       break;
     default:
       alert('Yes or No please!');
@@ -82,11 +87,12 @@ function getName(){
     answer = answer.toLowerCase();
     switch(answer){
     case 'no':
-      alert('Wrong. I graduated highscool in 2015.');
+      alert('Nice try! I graduated highscool in 2015.');
       break;
     case 'yes':
     //   console.log('Correct');
       alert('Correct. I graduated highschool in 2015.');
+      score = score + 1;
       break;
     default:
       alert('Yes or No please!');
@@ -105,9 +111,10 @@ function getName(){
     case 'no':
     //   console.log('Correct');
       alert('Correct! Attack on Titan is amazing, but Full Metal Alchemnist is my all time favorite anime.');
+      score = score + 1;
       break;
     case 'yes':
-      alert('Wrong! Attack on Titan is amazing, but Full Metal Alchemnist is my all time favorite anime.');
+      alert('Nice try! Attack on Titan is amazing, but Full Metal Alchemnist is my all time favorite anime.');
       break;
     default:
       alert('Yes or No please!');
@@ -129,6 +136,7 @@ function sixthQ(){
       alert('Please enter a number!');
     } else if(guess == answer){
       alert('Nice job! You guessed it!');
+      score = score + 1;
       i += 5;
     } else if(i >= 4){
       alert('Out of guesses! The correct number is ' + answer + '.');
@@ -147,4 +155,53 @@ function sixthQ(){
   }
 }
 
+function seventhQ(){
+  let game = ['breath of the wild', 'stardew valley', 'it takes two', 'skyrim' , 'portal', 'botw'];
+  let guess = null;
+  for(let i = 1; i < 7;){
+    guess = prompt('Guess one of top five game titles.');
+    guess = guess.toLocaleLowerCase();
+    switch(guess){
+    case game[0], game[6]:
+      alert('Correct!');
+      score = score + 1;
+      i += 7;
+      break;
+    case game[1]:
+      alert('Correct!');
+      score = score + 1;
+      i += 7;
+      break;
+    case game[2]:
+      alert('Correct!');
+      score = score + 1;
+      i += 7;
+      break;
+    case game[3]:
+      alert('Correct!');
+      score = score + 1;
+      i += 7;
+      break;
+    case game[4]:
+      alert('Correct!');
+      score = score + 1;
+      i += 7;
+      break;
+    default:
+      if(i < 6){
+        alert('Good guess! Not in my top five though! You have ' + (6 - i) + ' guess(es) left!');
+        i++;
+      }else{
+        alert('Out of guesses!');
+        i++;
+      }
+      break;
+    }
+  }
+  alert('Possible anwers: ' + game.join(', ') + '.');
+  alert('You got ' + score + '/7 answers correct.');
+}
 
+// if (guess == game[0] || guess == game[1] || guess == game[2] || guess == game[3] || guess == game[4] || guess == game[5]){
+
+// }
